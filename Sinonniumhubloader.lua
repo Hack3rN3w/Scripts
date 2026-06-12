88bc69a31fc30cf477c57e6a778a40b416510ef2
Hack3rN3w <DarrelShalgimbayev@gmail.com>
2026-06-12 14:39:09 +0500

    Update Sinonniumhubloader.lua

--- Sinonniumhubloader.lua
+++ Sinonniumhubloader.lua
@@ -1,15 +1,30 @@
 local url = "https://raw.githubusercontent.com/Hack3rN3w/Scripts/main/Sinonniumhub.lua"
+
 local success, code = pcall(function()
     return game:HttpGet(url)
 end)
 
 if success then
-    print("Succesful") -- если тут 0 или мало, значит файл не скачался
-    if #code > 0 then
-        loadstring(code)()
+    -- 1. Проверяем, не подсунул ли GitHub нам 404 ошибку вместо скрипта
+    if code:find("404: Not Found") then
+        warn("ОШИБКА 404: Файл 'Sinonniumhub.lua' не найден по этой ссылке!")
+    elseif #code > 0 then
+        
+        -- 2. Безопасно компилируем код БЕЗ моментального вызова скобками ()
+        local executableFunction, err = loadstring(code)
+        
+        -- 3. Если loadstring вернул функцию (а не nil), то запускаем её
+        if executableFunction then
+            print("Скрипт успешно запущен!")
+            executableFunction() 
+        else
+            -- Если внутри скачанного скрипта была синтаксическая ошибка
+            warn("Ошибка компиляции скрипта: " .. tostring(err))
+        end
+        
     else
         warn("Скрипт пустой!")
     end
 else
-    warn("Error of opening. Try changing executor: " .. tostring(code))
+    warn("Ошибка подключения. Попробуй сменить эксплоит: " .. tostring(code))
 end
