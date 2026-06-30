6cf45fe45f2bd8f722ace75398944c03c6eecd38
Hack3rN3w <DarrelShalgimbayev@gmail.com>
2026-06-30 22:52:15 +0500

    Create Universal scripthubloader.lua

---
+++ Universal scripthubloader.lua
@@ -0,0 +1,37 @@
+local starterGui = game:GetService("StarterGui")
+
+starterGui:SetCore("SendNotification", {
+    Title = "Loader",
+    Text = "Downloading the scripthub....",
+    Duration = 3
+})
+task.wait(3)
+
+starterGui:SetCore("SendNotification", {
+    Title = "Loader",
+    Text = "Success!",
+    Duration = 1
+})
+task.wait(1)
+
+starterGui:SetCore("SendNotification", {
+    Title = "Loader",
+    Text = "Checking the protection...",
+    Duration = 3
+})
+task.wait(3)
+
+starterGui:SetCore("SendNotification", {
+    Title = "Loader",
+    Text = "Success!",
+    Duration = 1
+})
+task.wait(1)
+
+local success, result = pcall(function()
+    return game:HttpGet("https://raw.githubusercontent.com/Hack3rN3w/Scripts/refs/heads/main/Universal%20scripthub.lua", true)
+end)
+
+if success and result then
+    loadstring(result)()
+end
